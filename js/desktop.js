e92150eb8314821925b1ac07c01273a41e367245
Vishalofficail5 <vishal05.official@gmail.com>
2026-07-07 18:56:46 +0530

    Final Release

--- js/desktop.js
+++ js/desktop.js
@@ -19,42 +19,42 @@ homeBtn.addEventListener("click", () => {
 // About
 const aboutBtn = document.getElementById("aboutBtn");
 const aboutWindow = document.getElementById("aboutWindow");
-aboutBtn.addEventListener("dblclick", () => openWindow(aboutWindow));
+aboutBtn.addEventListener("click", () => openWindow(aboutWindow));
 
 // Projects
 const projectsBtn = document.getElementById("projectBtn");
 const projectsWindow = document.getElementById("projectsWindow");
-projectsBtn.addEventListener("dblclick", () => openWindow(projectsWindow));
+projectsBtn.addEventListener("click", () => openWindow(projectsWindow));
 
 // Experience
 const experienceBtn = document.getElementById("experienceBtn");
 const experienceWindow = document.getElementById("experienceWindow");
-experienceBtn.addEventListener("dblclick", () => openWindow(experienceWindow));
+experienceBtn.addEventListener("click", () => openWindow(experienceWindow));
 
 // Contact
 const contactBtn = document.getElementById("contactBtn");
 const contactWindow = document.getElementById("contactWindow");
-contactBtn.addEventListener("dblclick", () => openWindow(contactWindow));
+contactBtn.addEventListener("click", () => openWindow(contactWindow));
 
 // Lotify (Song)
 const songBtn = document.getElementById("songBtn");
 const songWindow = document.getElementById("songWindow");
-songBtn.addEventListener("dblclick", () => openWindow(songWindow));
+songBtn.addEventListener("click", () => openWindow(songWindow));
 
 // Arcade (Game)
 const gameBtn = document.getElementById("gameBtn");
 const gameWindow = document.getElementById("gameWindow");
-gameBtn.addEventListener("dblclick", () => openWindow(gameWindow));
+gameBtn.addEventListener("click", () => openWindow(gameWindow));
 
 // Terminal
 const terminalBtn = document.getElementById("terminalBtn");
 const terminalWindow = document.getElementById("terminalWindow");
-terminalBtn.addEventListener("dblclick", () => openWindow(terminalWindow));
+terminalBtn.addEventListener("click", () => openWindow(terminalWindow));
 
 // Resume
 const resumeBtn = document.getElementById("resumeBtn");
 const resumeWindow = document.getElementById("resumeWindow");
-resumeBtn.addEventListener("dblclick", () => openWindow(resumeWindow));
+resumeBtn.addEventListener("click", () => openWindow(resumeWindow));
 
 document.getElementById("resumeViewBtn").addEventListener("click", () => {
     window.open("assets/Resume/Resume.pdf", "_blank");
